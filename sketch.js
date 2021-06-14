d195df79f1e043ccdb103245d79116b95a340dc8
arwysyah <arwysyahputrasiregar@gmail.com>
2021-06-14 17:55:08 +0700

    remove

--- sketch.js
+++ sketch.js
@@ -1,39 +1,43 @@
-let mobileNet;
-let video;
-var label;
-
-function modelReady() {
-  console.log("model Ready");
+var song;
+var amp;
+const canvas = 300;
+var mic;
+function preload() {
+  song = loadSound("bruno_mars.mp3");
 }
-function videoIsReady() {
-  console.log("run");
-  // image(video, 0, 0, width, height);
-  mobileNet.predict(gotResults);
+var isPlay = false;
+function setup() {
+  createCanvas(canvas, canvas);
+  amp = new p5.Amplitude();
+  slider = createSlider(1, 0, 1, 0.01);
+  button = createButton(getName());
+  button.position(0, 0);
+  button.mousePressed(changeBG);
 }
 
-function gotResults(error, res) {
-  if (error) {
-    console.log(error);
+function changeBG() {
+  let val = random(255);
+  if (song.isPlaying() === false) {
+    song.play();
+    isPlay = false;
   } else {
-    console.log(res);
-    label = res[0].label;
-
-    mobileNet.predict(gotResults);
+    song.stop();
+    isPlay = true;
   }
 }
-function setup() {
-  createCanvas(640, 500);
-  video = createCapture(VIDEO);
 
-  video.hide();
-  background(0);
-  mobileNet = ml5.imageClassifier("MobileNet", video, videoIsReady);
+function getName() {
+  const buttonName = song.isPlaying() === false ? "Play" : "Stop";
+  return buttonName;
+}
+function getBackGround() {
+  isPlay ? background(12) : background(0, 220, 0);
 }
 
 function draw() {
-  background(0);
-  image(video, 0, 0);
-  fill(255);
-  textSize(18);
-  text(label, 10, height - 5);
+  getBackGround();
+  song.setVolume(slider.value());
+  let vol = amp.getLevel();
+  console.log(vol);
+  ellipse(300 / 2, 150, canvas, vol * canvas);
 }
